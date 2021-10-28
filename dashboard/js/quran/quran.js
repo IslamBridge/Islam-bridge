65e832529832193f28cff2c29129add79f93cc9b
Abdullah Alhriri <alhariri.abdu.2018@gmail.com>
2021-10-28 09:16:38 +0200

    quran page tweaks/update

--- dashboard/js/quran/quran.js
+++ dashboard/js/quran/quran.js
@@ -4,9 +4,8 @@ const allChaptersBox = document.getElementById("allSurahat");
 const searchBox = document.getElementById("searchBox");
 
 const makeChapterCardHtml = (el) => {
-  console.log(el);
-  return `<a href="/chapter/${el.id}" class="surah col-lg-4  col-md-6 col-sm-12">
-  <div class="surah-card mt-3">
+  return `<a href="/chapter/${el.id}" class="surah col-lg-4  col-md-6 col-sm-12 mt-3">
+  <div class="surah-card ">
       <div class="surah-card__number">${el.id}</div>
           <div class="surah-card__text">
               <div class="surah-card__latin">
@@ -21,7 +20,6 @@ const makeChapterCardHtml = (el) => {
 };
 
 const printQuranChapters = async function (quranChapters) {
-  // console.log(quranChapters);
   const quranChaptersHtml = quranChapters.map((el) => makeChapterCardHtml(el)).join("");
   allChaptersBox.insertAdjacentHTML("afterbegin", quranChaptersHtml);
   spinner.classList.add("d-none");
@@ -46,7 +44,8 @@ const search = async (quranChapters) => {
           .replaceAll(" ", "")
           .replaceAll("-", "")
           .toLowerCase()
-          .split(searchBox.value.replaceAll(" ", "").toLowerCase()).length >= 2
+          .split(searchBox.value.replaceAll(" ", "").toLowerCase()).length >= 2 ||
+        (el.id + "").split(searchBox.value.replaceAll(" ", "").toLowerCase()).length >= 2
       ) {
         allChaptersBox.insertAdjacentHTML("beforeEnd", makeChapterCardHtml(el));
       }
@@ -73,115 +72,3 @@ const init = async () => {
 };
 
 init();
-
-/* 
-  {
-    "id": 1,
-    "name": "الفاتحة",
-    "transliteration": "Al-Fatihah",
-    "type": "meccan",
-    "total_verses": 7,
-    "link": "https://cdn.jsdelivr.net/npm/quran-json@3.1.2/dist/chapters/1.json"
-  }
-  {
-  "id": 1,
-  "name": "الفاتحة",
-  "transliteration": "Al-Fatihah",
-  "type": "meccan",
-  "total_verses": 7,
-  "verses": [
-    {
-      "id": 1,
-      "text": "بِسۡمِ ٱللَّهِ ٱلرَّحۡمَٰنِ ٱلرَّحِيمِ",
-      "transliteration": "Bismi Allahi alrrahmani alrraheemi"
-    },
-    {
-      "id": 2,
-      "text": "ٱلۡحَمۡدُ لِلَّهِ رَبِّ ٱلۡعَٰلَمِينَ",
-      "transliteration": "Alhamdu lillahi rabbi alAAalameena"
-    },
-    {
-      "id": 3,
-      "text": "{
-  "id": 1,
-  "name": "الفاتحة",
-  "transliteration": "Al-Fatihah",
-  "type": "meccan",
-  "total_verses": 7,
-  "verses": [
-    {
-      "id": 1,
-      "text": "بِسۡمِ ٱللَّهِ ٱلرَّحۡمَٰنِ ٱلرَّحِيمِ",
-      "transliteration": "Bismi Allahi alrrahmani alrraheemi"
-    },
-    {
-      "id": 2,
-      "text": "ٱلۡحَمۡدُ لِلَّهِ رَبِّ ٱلۡعَٰلَمِينَ",
-      "transliteration": "Alhamdu lillahi rabbi alAAalameena"
-    },
-    {
-      "id": 3,
-      "text": "ٱلرَّحۡمَٰنِ ٱلرَّحِيمِ",
-      "transliteration": "Alrrahmani alrraheemi"
-    },
-    {
-      "id": 4,
-      "text": "مَٰلِكِ يَوۡمِ ٱلدِّينِ",
-      "transliteration": "Maliki yawmi alddeeni"
-    },
-    {
-      "id": 5,
-      "text": "إِيَّاكَ نَعۡبُدُ وَإِيَّاكَ نَسۡتَعِينُ",
-      "transliteration": "Iyyaka naAAbudu waiyyaka nastaAAeenu"
-    },
-    {
-      "id": 6,
-      "text": "ٱهۡدِنَا ٱلصِّرَٰطَ ٱلۡمُسۡتَقِيمَ",
-      "transliteration": "Ihdina alssirata almustaqeema"
-    },
-    {
-      "id": 7,
-      "text": "صِرَٰطَ ٱلَّذِينَ أَنۡعَمۡتَ عَلَيۡهِمۡ غَيۡرِ ٱلۡمَغۡضُوبِ عَلَيۡهِمۡ وَلَا ٱلضَّآلِّينَ",
-      "transliteration": "Sirata allatheena anAAamta AAalayhim ghayri almaghdoobi AAalayhim wala alddalleena"
-    }
-  ]
-}",
-      "transliteration": "Alrrahmani alrraheemi"
-    },
-    {
-      "id": 4,
-      "text": "مَٰلِكِ يَوۡمِ ٱلدِّينِ",
-      "transliteration": "Maliki yawmi alddeeni"
-    },
-    {
-      "id": 5,
-      "text": "إِيَّاكَ نَعۡبُدُ وَإِيَّاكَ نَسۡتَعِينُ",
-      "transliteration": "Iyyaka naAAbudu waiyyaka nastaAAeenu"
-    },
-    {
-      "id": 6,
-      "text": "ٱهۡدِنَا ٱلصِّرَٰطَ ٱلۡمُسۡتَقِيمَ",
-      "transliteration": "Ihdina alssirata almustaqeema"
-    },
-    {
-      "id": 7,
-      "text": "صِرَٰطَ ٱلَّذِينَ أَنۡعَمۡتَ عَلَيۡهِمۡ غَيۡرِ ٱلۡمَغۡضُوبِ عَلَيۡهِمۡ وَلَا ٱلضَّآلِّينَ",
-      "transliteration": "Sirata allatheena anAAamta AAalayhim ghayri almaghdoobi AAalayhim wala alddalleena"
-    }
-  ]
-}
-
-<a href="/index" class="surah col-lg-4 col-sm-12">
-    <div class="surah-card mt-3">
-        <div class="surah-card__number">index</div>
-            <div class="surah-card__text">
-                <div class="surah-card__latin">
-                    <h5>surah name</h5>
-                    <div class="ayah-number-wrapper">
-                    <p class="english">meaning surah name</p>
-                </div>
-            </div>
-        </div>
-    </div>
-</a> 
-*/
